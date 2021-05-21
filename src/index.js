bf2cc5d5344d06e6f3c9bd7f10d48da0ff4ef5aa
hotcakedev628 <hotcake.dev.628@hotmail.com>
2021-05-20 19:09:14 -0700

    build settings context & import

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
+import 'react-perfect-scrollbar/dist/css/styles.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { SettingsProvider } from './contexts/SettingsContext';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <SettingsProvider>
     <App />
-  </React.StrictMode>,
+  </SettingsProvider>,
   document.getElementById('root')
 );
 
